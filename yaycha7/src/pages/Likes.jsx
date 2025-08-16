2929f8c03fbe943d1a0b5e670f5cf97502a2a5e8
kaungmyatkyawtiti <eucalyptustiti2003@gmail.com>
2025-08-16 23:53:25 +0630

    My JS Testings

--- yaycha7/src/pages/Likes.jsx
+++
@@ -1,9 +0,0 @@
-import UserList from "../components/UserList.jsx";
-
-import { Box } from "@mui/material";
-
-export default function Likes() {
-  return <Box>
-    <UserList title="likes" />
-  </Box>
-}
