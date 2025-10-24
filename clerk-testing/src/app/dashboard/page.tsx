e571a06574b3232e4d7baa5f319abc782be8c900
kaungmyatkyawtiti <eucalyptustiti2003@gmail.com>
2025-10-24 21:37:03 +0630

    chore(js-test): init project to github

--- clerk-testing/src/app/dashboard/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function DashboardPage() {
-  return (
-    <h1>
-      This is dashboard page
-    </h1>
-  )
-}
--- clerk-testing/src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div className="text-red-400">
-      This is Home Page
-    </div>
-  );
-}
